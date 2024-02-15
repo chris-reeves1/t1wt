fe26ad673a4404bc9adde8f52a3c8b3b8fc414eb
chris-reeves1 <91483522+chris-reeves1@users.noreply.github.com>
2024-02-15 08:05:36 +0000

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -2,7 +2,7 @@ import requests
 import unittest
 
 class FlaskAppTests(unittest.TestCase):
-    BASE_URL = "http://localhost:3000"
+    BASE_URL = "http://localhost:80"
 
     def test_home_page(self):
         response = requests.get(f"{self.BASE_URL}/")
